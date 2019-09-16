555f0afa7fce24cac0fb137711bf1b05256f7566
carpentre1 <jlowell.carpenter@gmail.com>
2019-09-16 14:58:47 -0500

    Initial outline of planned work

--- SortingAlgorithms/Sort.cs
+++ SortingAlgorithms/Sort.cs
@@ -4,7 +4,43 @@ using System.Text;
 
 namespace SortingAlgorithms
 {
-    class Sort
+    /// <summary>
+    /// Use this commenting structure for each method. two references for each sorting method
+    /// 8 total methods
+    /// Read in a csv file as the input to your sorting methods, File.ReadLine to read one line of data at a time
+    /// Example URL inclusion:
+    /// Justin doing: Shell Sort, Radix Sort
+    /// <see cref="url"/>
+    /// /// <see cref="url"/>
+    /// </summary>
+    public static class Sort
     {
+        /// <summary>
+        /// Justin's Radix Sorts
+        /// </summary>
+        /// <param name="theData"></param>
+        /// <returns></returns>
+        public static List<Guid> RadixSort(List<Guid> theData)
+        {
+            return new List<Guid>();
+        }
+        public static List<Double> RadixSort(List<Double> theData)
+        {
+            return new List<Double>();
+        }
+
+        /// <summary>
+        /// Justin's Shell Sorts
+        /// </summary>
+        /// <param name="theData"></param>
+        /// <returns></returns>
+        public static List<Guid> ShellSort(List<Guid> theData)
+        {
+            return new List<Guid>();
+        }
+        public static List<Double> ShellSort(List<Double> theData)
+        {
+            return new List<Double>();
+        }
     }
 }
