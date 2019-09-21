7ff4273d4708dec489f66de703289bae7b19bbf3
carpentre1 <jlowell.carpenter@gmail.com>
2019-09-21 16:42:30 -0500

    Shell sort and guid comparison
    Guid comparison determines if the first argument guid is greater, for the purposes of sorting guids in shell sort

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -8,15 +8,18 @@ namespace SortingAlgorithms
         {
             Console.WriteLine("Hello World!");
             Sort.FileReader();
-            Console.WriteLine("Finished reading.");
-            Console.WriteLine(Sort.theInts[999999] + "," + Sort.theGuids[999999] + "," + Sort.theDoubles[999999]);//testing to make sure it can tell us the final line of the CSV
+            Console.WriteLine("Finished reading file.");
+            Console.WriteLine("Final int: " + Sort.theInts[999999] + "," + " final guid: " + Sort.theGuids[999999] + "," + " final double: " + Sort.theDoubles[999999]);//testing to make sure it can tell us the final line of the CSV
 
-            Console.WriteLine("Shell Sort:");
-            Sort.exampleShellSort(Sort.theDoubles, Sort.doublesListLength);
-            Sort.ShowElementsOfList(Sort.theDoubles);
-
-            Console.WriteLine("Done.");
-            Console.Read();
+            Console.WriteLine("Press enter to begin shell sort.");
+            Console.ReadLine();
+            //Sort.exampleShellSort(Sort.theDoubles, Sort.doublesListLength);
+            Sort.ShellSort(Sort.theGuids);
+            Console.WriteLine("Done sorting. Press enter to show sorted list.");
+            Console.ReadLine();
+            Sort.ShowElementsOfList(Sort.theGuids);
+            Console.WriteLine("Done showing list. Press enter to exit program.");
+            Console.ReadLine();
 
 
         }
--- SortingAlgorithms/Sort.cs
+++ SortingAlgorithms/Sort.cs
@@ -80,44 +80,82 @@ namespace SortingAlgorithms
         /// Currently implemented shell sort is a total cut and paste, just to examine functionality!
         /// From: <see cref="https://www.tutorialspoint.com/shell-sort-program-in-chash"/>
         /// From: <see cref="https://www.w3resource.com/csharp-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-1.php"/>
+        /// From: <see cref="https://www.geeksforgeeks.org/shellsort/"/>
         /// </summary>
         /// <param name="theData"></param>
         /// <returns></returns>
         /// 
-        public static void exampleShellSort(List<Double> arr, int list_length)
+
+        public static int ShellSort(List<Double> dList)//Time complexity: O(N^2). Gap is reduced by half every iteration. too few gaps slow down the passes, and too many gaps produces overhead.
         {
-            int i, j, inc, temp;
-            inc = 3;
-            while (inc > 0)
+            int n = dList.Count;//n is the size of our list
+
+            // Sort using large gaps, and reduce gap size gradually
+            for (int gap = n / 2; gap > 0; gap /= 2)//our gaps will be 500k, then 250k, then 125k, etc... until
             {
-                Console.WriteLine("A");
-                for (i = 0; i < list_length; i++)
+                // Perform insertion sorts using the current gap size
+                // The first gap elements dList[0..gap-1] are already 
+                // in gapped order keep adding one more element 
+                // until the entire array is gap sorted 
+                for (int i = gap; i < n; i += 1)
                 {
-                    Console.WriteLine(i);
-                    j = i;
-                    temp = (int)arr[i];
-                    while ((j >= inc) && (arr[j - inc] > temp))
-                    {
-                        arr[j] = arr[j - inc];
-                        j = j - inc;
-                    }
-                    arr[j] = temp;
+                    // add dList[i] to the elements that have 
+                    // been gap sorted save dList[i] in temp and 
+                    // make a hole at position i 
+                    double temp = dList[i];
+
+                    // shift earlier gap-sorted elements up until 
+                    // the correct location for dList[i] is found 
+                    int j;
+                    for (j = i; j >= gap && dList[j - gap] > temp; j -= gap)
+                        dList[j] = dList[j - gap];
+
+                    // put temp (the original dList[i])  
+                    // in its correct location 
+                    dList[j] = temp;
                 }
-                if (inc / 2 != 0)
-                    inc = inc / 2;
-                else if (inc == 1)
-                    inc = 0;
-                else
-                    inc = 1;
             }
+            return 0;
         }
-        public static List<Guid> ShellSort(List<Guid> theData)
+        /// <summary>
+        /// For the sake of comparing guids, this function returns true when the first guid is greater than the second, determined by the Guid.CompareTo method.
+        /// Guid.CompareTo's documentation on how it compares:
+        /// The CompareTo method compares the GUIDs as if they were values provided to the Guid(Int32, Int16, Int16, Byte[]) constructor, as follows:
+        /// It compares the UInt32 values, and returns a result if they are unequal. If they are equal, it performs the next comparison.
+        /// It compares the first UInt16 values, and returns a result if they are unequal. If they are equal, it performs the next comparison.
+        /// It compares the second UInt16 values, and returns a result if they are unequal. If they are equal, it performs the next comparison.
+        /// If performs a byte-by-byte comparison of the next eight Byte values. When it encounters the first unequal pair, it returns the result.
+        /// Otherwise, it returns 0 to indicate that the two Guid values are equal.
+        /// 
+        /// In its example, it demonstrates that a value of -1 is "less than", 0 is "equals", and 1 is "greater than".
+        /// </summary>
+        static bool Guid1IsGreater(Guid guid1, Guid guid2)
         {
-            return new List<Guid>();
+            if (guid1.CompareTo(guid2) == 1)
+                return true;
+            else
+                return false;
         }
-        public static List<Double> ShellSort(List<Double> theData)
+        public static int ShellSort(List<Guid> gList)
         {
-            return new List<Double>();
+            int n = gList.Count;
+
+            for (int gap = n / 2; gap > 0; gap /= 2)
+            {
+
+                for (int i = gap; i < n; i += 1)
+                {
+                    Guid temp = gList[i];
+
+
+                    int j;
+                    for (j = i; j >= gap &&  Guid1IsGreater(gList[j - gap], temp); j -= gap)
+                        gList[j] = gList[j - gap];
+
+                    gList[j] = temp;
+                }
+            }
+            return 0;
         }
     }
 }
